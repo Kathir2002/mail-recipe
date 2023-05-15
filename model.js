eb46515359792e4a80f1a816baae1c48952390dd
kathir <kathir@centiizen.com>
2023-05-15 14:11:52 +0530

    fimit

---
+++ model.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      maxlength: 32,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      index: { unique: true },
+      match: /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    userRole: {
+      type: Number,
+      required: true,
+    },
+    phoneNumber: {
+      type: Number,
+    },
+    userImage: {
+      type: String,
+      default: "https://cdn-icons-png.flaticon.com/512/149/149071.png",
+    }
+  },
+  { timestamps: true }
+);
+
+const userModel = mongoose.model("users", userSchema);
+module.exports = userModel;
